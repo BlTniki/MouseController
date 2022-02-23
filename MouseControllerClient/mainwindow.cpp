e9ba84ed562c2c6727f63d3dfcb0be4fda7ab310
BlTniki <99419239+BlTniki@users.noreply.github.com>
2022-02-23 12:17:31 +0300

    Update mainwindow.cpp

--- MouseControllerClient/mainwindow.cpp
+++ MouseControllerClient/mainwindow.cpp
@@ -5,23 +5,19 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    //Before connection blocking all objects, that communicate with the server
     ui->setupUi(this);
-    ui->pB_Send->setEnabled(false);
-    ui->senseArea->blockSignals(true);
-    ui->pB_LeftClick->setEnabled(true);
-    ui->pB_MiddleClick->setEnabled(true);
-    ui->pB_RightClick->setEnabled(true);
-    ui->pB_LeftClick->setEnabled(false);
-    ui->pB_MiddleClick->setEnabled(false);
-    ui->pB_RightClick->setEnabled(false);
+
+    ui->InputWindow->hide();
 
     sa = ui->senseArea;
     sb = ui->scrollBar;
-
     connect(sa, &SenseArea::TouchRecived, this, &MainWindow::TouchMove);
     connect(sa, &SenseArea::ClickRecived, this, &MainWindow::ClickDo);
     connect(sb, &ScrollBar::TouchRecived, this, &MainWindow::ScrollMove);
+
+    UDPsocket = new QUdpSocket(this);
+    UDPsocket->connectToHost("192.168.31.193", 2323);
+    connect(UDPsocket, &QUdpSocket::disconnected, UDPsocket, &QUdpSocket::deleteLater);
 }
 
 MainWindow::~MainWindow()
@@ -43,34 +39,24 @@ void MainWindow::on_pB_Send_clicked()
  **/
 void MainWindow::on_pB_Connect_clicked() //After click on connect button we create new socket, connect signals and connect to host
 {
-    socket = new QTcpSocket(this);
-    connect(socket, &QTcpSocket::connected, this, &MainWindow::connectResived);
-    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
-    connect(socket, &QTcpSocket::disconnected, this, &MainWindow::disconnectResived);
+    TCPsocket = new QTcpSocket(this);
+    connect(TCPsocket, &QTcpSocket::connected, this, &MainWindow::connectResived);
+    connect(TCPsocket, &QTcpSocket::disconnected, TCPsocket, &QTcpSocket::deleteLater);
+    connect(TCPsocket, &QTcpSocket::disconnected, this, &MainWindow::disconnectResived);
 
-    socket->connectToHost("192.168.31.193", 2323);
+    TCPsocket->connectToHost("192.168.31.193", 2323);
 }
 
 void MainWindow::connectResived()// Unblocking all objects, that communicate with the server
 {
-    ui->senseArea->blockSignals(false);
-    ui->scrollBar->blockSignals(false);
-
-    ui->pB_Send->setEnabled(true);
-    ui->pB_LeftClick->setEnabled(true);
-    ui->pB_MiddleClick->setEnabled(true);
-    ui->pB_RightClick->setEnabled(true);
+    ui->ConnectWindow->hide();
+    ui->InputWindow->show();
 }
 
 void MainWindow::disconnectResived()// Blocking all objects, that communicate with the server
 {
-    ui->senseArea->blockSignals(true);
-    ui->scrollBar->blockSignals(true);
-
-    ui->pB_Send->setEnabled(false);
-    ui->pB_LeftClick->setEnabled(false);
-    ui->pB_MiddleClick->setEnabled(false);
-    ui->pB_RightClick->setEnabled(false);
+    ui->ConnectWindow->show();
+    ui->InputWindow->hide();
 }
 
 void MainWindow::SendToServer(MsgType type, QString str)// Sending to server str and MsgType
@@ -84,7 +70,7 @@ void MainWindow::SendToServer(MsgType type, QString str)// Sending to server str
     out.device()->seek(0);
     out << quint16(data.size()-sizeof(quint16));
 
-    socket->write(data);
+    UDPsocket->write(data);
 }
 
 
